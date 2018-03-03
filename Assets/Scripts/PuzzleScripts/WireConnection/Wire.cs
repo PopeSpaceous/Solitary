ba6b787252e6f1605782be239fa5dff291dbd6ad
Leonel Jara <leoneljara3@gmail.com>
2018-03-03 13:48:45 -0500

    PrimDeployment: WireConnection
    * WireConnection v1
    
    * WireConnection v2
    
    * Reworked WireConnection v3
    
    * WireConnection v4
    
    * WireConnection v5 Final v1
    
    * PuzzleConnection v5 Final 2: Wires Snap Back when wrong
    
    * WireConnection vFinal Added numbers and adjusted influ
    
    * Cleaned up static var when unloading the WC scene
    
    * Added Wire Asset and adjusted scaling of wire. Also, modified the difficulty scale
    
    * Added Connection Asset

---
+++ Assets/Scripts/PuzzleScripts/WireConnection/Wire.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author: Leonel Jara
+public class Wire : MonoBehaviour {
+
+	public int wireIDLink = 0; // will be assgined by InitPuzzle()
+	public Connection connection = null;
+	
+	public float scaleLimit = 20f; // limit the scale. so its does not scale infinitely
+
+    public GameObject neckGO; // the ref of the neck wire
+
+	private Camera cameraTarget; // Change this if u remove the puzzle camera
+
+    //original positions for the head and neck of the wire
+	private Vector2 orgWireHeadWidth;
+    private Vector2 orgWireNeckWidth;
+
+    //current ref of the head and neck of the wire
+    private Transform wireHeadTrans;
+    private Transform wireNeckTrans;
+
+    //ref of the neck and head for the sprite
+    private SpriteRenderer wireSpriteHead;
+    private SpriteRenderer wireSpriteNeck;
+
+    private Color[] color = { Color.green, Color.gray, Color.white, Color.yellow, Color.red };
+
+    void Awake(){
+        wireSpriteNeck = neckGO.GetComponent<SpriteRenderer>();
+        wireSpriteHead = GetComponent<SpriteRenderer>();
+        wireNeckTrans = neckGO.GetComponent<Transform>();
+        wireHeadTrans = GetComponent<Transform>();
+        cameraTarget = GameObject.Find ("PuzzleCamera").GetComponent<Camera> (); // TODO: maybe have puzzlecamera have its own static instance
+	}
+
+	// Use this for initialization
+	void Start () {
+		orgWireHeadWidth = new Vector2 (wireSpriteHead.size.x, wireSpriteHead.size.y);
+        orgWireNeckWidth = new Vector2(wireSpriteNeck.size.x, wireSpriteNeck.size.y);
+
+        wireSpriteNeck.color = color[wireIDLink - 1];
+    }
+
+
+	void OnMouseDrag(){
+		//get mouse position that reps in the game world
+		Vector3 mousePoint = cameraTarget.ScreenToWorldPoint (Input.mousePosition);
+		//Follow the mouse
+		FollowTarget (mousePoint);
+	}
+
+	void OnMouseUp(){
+		//if the wire is not connected it will snap back to its orignal scale and rotation
+		if( connection == null ){
+			SnapWireBack ();
+		}
+		else {
+			//if the wire is connected and has not been disconnected; snap to the connection block.
+			connection.SnapWire ();
+		}
+	}
+	//When the player mouse down on the wire that is already connected this will causes it to disconnect	
+	void OnMouseDown(){
+		if(connection != null){
+			connection.DisconnectWireAffect ();
+		}
+	}
+	//Place the wire to its orginal position
+	public void SnapWireBack(){
+		wireSpriteHead.size = new Vector2 (orgWireHeadWidth.x ,orgWireHeadWidth.y);
+        wireSpriteNeck.size = new Vector2(orgWireNeckWidth.x, orgWireNeckWidth.y);
+        wireHeadTrans.rotation = Quaternion.Euler (new Vector3(0 , 0, 0));
+        wireNeckTrans.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        connection = null;
+	}
+
+	//Given the position, the wire will scale on the x axis and rotate to that position
+	public void FollowTarget(Vector3 targetPos){
+		
+		//rotation to follow 
+		Vector3 toTargetVector = targetPos - wireHeadTrans.position;
+		float zRotation = Mathf.Atan2( toTargetVector.y, toTargetVector.x ) * Mathf.Rad2Deg;
+		wireHeadTrans.rotation = Quaternion.Euler(new Vector3 ( 0, 0, zRotation));
+        wireNeckTrans.rotation = Quaternion.Euler(new Vector3(0, 0, zRotation ));
+
+        //Scale to follow
+        float distance = Vector2.Distance(wireHeadTrans.position,  targetPos); // calculates the magnitude between the mouse and this gameobject
+		float widthScaleChange = Mathf.Abs(distance) * 1.2f; // offset the value by 11.5f. This is so it can scale with the distance.
+
+        //will scale only if the scale change is not less then the current scale. And not passing the limit scale
+        if (widthScaleChange <= scaleLimit && widthScaleChange >= orgWireNeckWidth.x)
+        {
+            //Apply scale
+            wireSpriteHead.size = new Vector2(widthScaleChange + 0.8f, wireSpriteHead.size.y);
+            wireSpriteNeck.size = new Vector2(widthScaleChange, wireSpriteNeck.size.y);
+
+        }
+        else if(widthScaleChange < orgWireNeckWidth.x) {
+            wireSpriteHead.size = new Vector2(orgWireHeadWidth.x, orgWireHeadWidth.y);
+            wireSpriteNeck.size = new Vector2(orgWireNeckWidth.x, orgWireNeckWidth.y);
+        }
+	}
+		
+}
--- Assets/Scripts/PuzzleScripts/WireConnection/WireConnection.cs
+++ Assets/Scripts/PuzzleScripts/WireConnection/WireConnection.cs
@@ -2,18 +2,171 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+//Author: Leonel Jara
 public class WireConnection: Puzzle {
+
+	public Transform[] wirePlaceholders;
+	public Transform[] connectionPlaceholders;
+	public Transform[] lockPlaceholders;
+
+	public Text[] currentNumberText;
+	public Text[] neededNumberText;
+
+	//The puzzle canvus transfrom ref so we can still make the game object 
+    //as child of Puzzle Canvus gameobject
+	public Transform puzzleCan;
+
+	//Prefabs
+	public GameObject connectionPrefab;
+	public GameObject lockPrefab;
+	public GameObject wirePrefab;
+
+
+	//These are the list of refs for all locks, connections, and wires.
+	List<Wire> wires; 
+	List<Lock> realLocks; 
+	List<Connection> inputConnections; 
+
+	//The number of wires, connections, and locks there will be;
+	private int diffLength;
 
 	// Sets the parent fields
 	void Awake () {
 		puzzleName = "WireConnection";
+		//#if NO_DEBUG
 		difficulty = NextSceneManager.instance.setPuzzledifficulty;
 		placeholder = NextSceneManager.instance.placeholder;
+
 		Debug.Log ("Difficulty for puzzle " + puzzleName + " is: "+ this.difficulty);
+		//#endif
+
+		//Debuging---
+		//difficulty = 3;
+        //
+
+
+        //Makes sure it gets the right difficulty
+        if (difficulty > 3 || difficulty < 1)
+        {
+            Debug.LogError("Difficulty must be in the range of 1-3!");
+        }
+        else {
+           
+            //set diff length
+            if (difficulty == 3)
+            {
+                
+                diffLength = difficulty + 2;
+            }
+            else 
+            {
+                diffLength = difficulty + 3;
+            }
+
+
+            //init Lists
+            wires = new List<Wire>();
+            inputConnections = new List<Connection>();
+            realLocks = new List<Lock>();
+
+            InitPuzzle();
+        }
+
 	}
 
 
-	/* Your wonderful startup puzzle code here :3 */
+	void InitPuzzle(){
+		
+
+		//SetupWires--
+		for(int ctr = 0; ctr < diffLength; ctr++){
+			//Create Object and place it in the given placeholder
+			GameObject obj = Instantiate (wirePrefab,  puzzleCan) as GameObject;
+			//Set the position
+			obj.transform.position = wirePlaceholders [ctr].position;
+			//Get the Wire Script
+			Wire w = obj.GetComponentInChildren<Wire>();
+			//Assgin the var id in the wire script
+			w.wireIDLink = ctr + 1;
+			//Add it to our list collection
+			wires.Add(w);
+
+		}
+
+        //Shuffle the wires--
+        Shuffle<Wire>(wires);
+        //Setup Locks--
+
+        for (int ctr = 0; ctr < diffLength; ctr++) {
+			GameObject obj = Instantiate (lockPrefab,  puzzleCan) as GameObject;
+			//Set the position
+			obj.transform.position = lockPlaceholders [ctr].position;
+			//Get scripts attached to the gameobject
+			Lock r = obj.GetComponent<Lock> ();
+			//Set the ids
+			r.lockID = ctr + 1;
+			//Add it to our list collection
+			realLocks.Add (r);
+
+            //calulate needed sum
+            r.neededSum = wires[ctr].wireIDLink + r.lockID;
+            //Set Text UI
+            if (difficulty == 3)
+            {
+                neededNumberText[ctr].text = "?";
+            }
+            else {
+                neededNumberText[ctr].text = r.neededSum.ToString();
+            }
+            
+            r.textCSum = currentNumberText[ctr];
+        }
+
+
+		//Setup Connections--
+		for(int ctr = 0; ctr < diffLength; ctr++){
+			//Create Object and place it in the given placeholder
+			GameObject obj = Instantiate (connectionPrefab,  puzzleCan) as GameObject;
+			//Set the position
+			obj.transform.position = connectionPlaceholders [ctr].position;
+			//Get the Connection Script
+			Connection c = obj.GetComponentInChildren<Connection>();
+			//set values
+			c.assginedLock = realLocks [ctr];
+			//add it to collection
+			inputConnections.Add (c);
+		}
+
+	}
+    //This update function will check if all locks are open.If 
+    //they are it will call PuzzleComplete()
+	void Update(){
+
+		bool hasAllOpen = true;
+		foreach (var aLock in realLocks) {
+			if(!aLock.isOpen){
+				hasAllOpen = false;
+            }
+		}
+
+		if(hasAllOpen == true){
+			//Debug.Log ("WireConnection Puzzle Complete");
+            PuzzleComplete ();
+            Lock.unlocked.Clear(); // cleanup the static var. When unloading a scene a static var is not unloaded
+        }
+	}
+    //WIll shuffle the given array
+	private void Shuffle<T>(List<T> array) {
+		var count = array.Count;
+		var last = count - 1;
+		for (var i = 0; i < last; ++i) {
+			var r = UnityEngine.Random.Range(i, count);
+			var tmp = array[i];
+			array[i] = array[r];
+			array[r] = tmp;
+		}
+	}
 
 }
