e56d6666147b31d42638f73745322e3f6aca1475
Jyrah <Jyrahscope@gmail.com>
2018-03-03 14:04:18 -0500

    woops

---
+++ Assets/Scripts/PuzzleScripts/ImageScramble/ImageController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageController : MonoBehaviour {
+
+    public GameObject target;
+    public bool startMove = false;
+    ImageScramble gameMN;
+
+	// Use this for initialization
+	void Start () {
+        GameObject gamemanager = GameObject.Find ("ImageScramble");
+        gameMN = gamemanager.GetComponent<ImageScramble> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startMove) {
+            startMove = false;
+            this.transform.position = target.transform.position; // move to new position
+            gameMN.checkComplete = true;
+        }
+	}
+}
---
+++ Assets/Scripts/PuzzleScripts/ImageScramble/ImageScrambleMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageScrambleMovement : MonoBehaviour {
+
+    public int row, col;
+    ImageScramble gameMN;
+
+    // Use this for initialization
+    void Start () {
+        GameObject gamemanager = GameObject.Find ("ImageScramble");
+        gameMN = gamemanager.GetComponent<ImageScramble> ();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown () {
+        Debug.Log ("Row is : " + row + " Col is: " + col); // test
+        gameMN.countStep += 1;
+        gameMN.row = row;
+        gameMN.col = col;
+        gameMN.startControl = true;
+    }
+}
