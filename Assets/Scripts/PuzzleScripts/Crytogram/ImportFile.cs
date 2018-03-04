27646b8ee1189909dcaccd592b2aee1c51abfc03
PopeSpaceous <nathan_misener@hotmail.com>
2018-03-04 15:01:42 -0500

    initial Cyrptogram scripts

---
+++ Assets/Scripts/PuzzleScripts/Crytogram/ImportFile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ImportFile : MonoBehaviour {
+
+	public string getStringFromFile (){
+		//textString holds the string data from the file which will be split by delimeter
+		//retString is the string we will return to Start() function
+		string textString="", retString;
+		//Opens a stream reader
+		StreamReader sr;
+		//check the difficulty of the puzzle
+		sr = new StreamReader ("5LWords.csv");
+	
+		//grab each line in the file
+		do {
+			textString+=sr.ReadLine();
+		} while (sr.Peek () != -1);
+		//close the stream reader
+		sr.Close ();
+		//clone the split string by ';' delim
+		string[] parsedString = (string[]) (textString.Split(',')).Clone();
+		//get a random integer between 0 and thelength(exclusively) of the string
+		int r = (int)(Random.Range(0, (float)parsedString.Length));
+		//Get the random tan coordinate from the string array
+		retString = parsedString [(int)r];
+		return retString;
+	}
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/PuzzleScripts/Crytogram/cipher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cipher : MonoBehaviour {
+	List<char> alpha= new List<char>();
+	//65-90
+	// Use this for initialization
+	void Start () {
+		for (int x = 65; x < 96; x++) {
+			alpha.Add (x);
+		}
+
+
+		//Get word from file
+
+
+	}
+	
+
+}
