e8128f68b23278cad0b84c68247e84a807cb3d04
Leonel Jara <leoneljara3@gmail.com>
2018-03-16 16:15:15 -0400

    Added Insturctions Panel prefab for puzzle

---
+++ Assets/Scripts/PuzzleScripts/ExitInstructions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/*
+ * This class will be used for exiting the insturctions panel
+ * 
+*/
+public class ExitInstructions : MonoBehaviour {
+
+    public Button exitButton;
+
+    public GameObject parent;
+
+    void Start () {
+        Button btn = exitButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    //unactivate the partent gameobject attched to this script
+    public void TaskOnClick() {
+        parent.SetActive(false);
+    }
+}
