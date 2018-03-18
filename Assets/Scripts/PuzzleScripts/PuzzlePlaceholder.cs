dffbf61abb3d6d997b3d2e33c4fed215a36f0d11
Leonel Jara <leoneljara3@gmail.com>
2018-03-18 14:01:14 -0400

    Player Walk, Jump, PuzzleStart Animations  & Adjusted start / end puzzle call chain (#19)

--- Assets/Scripts/PuzzleScripts/PuzzlePlaceholder.cs
+++ Assets/Scripts/PuzzleScripts/PuzzlePlaceholder.cs
@@ -14,26 +14,67 @@ public class PuzzlePlaceholder : MonoBehaviour {
 
 	public Level_Door door = null;
 
-	[HideInInspector]
-	public string puzzleGoTo;
+    //use for setting the appropriate set when a puzzle is completed
+    [HideInInspector]
+    public Level level; //Must be set by PuzzleRandomization class
+
+    //To what puzzle this placeholder must go to
+    [HideInInspector]
+	public string puzzleGoTo= "PuzzleTemplate";
+
 
 
 	void OnTriggerStay2D( Collider2D col)
 	{
-		if(col.gameObject.name.Equals("Player(Clone)") && Player.instance.actionButtion && Player.instance.canPlayerMove){
+		if(col.gameObject.CompareTag("Player") && Player.instance.actionButtion && !Player.instance.isInPuzzle)
+        {
 			//Lock the player's movement
-			Player.instance.canPlayerMove = false;
+			Player.instance.ChangeMovementLock(false);
+            //set the bool that the player is in a puzzle
+            Player.instance.isInPuzzle = true;
+            //Give the placeholder ref to player. So when animation is ready to start the puzzle it will call GoToPuzzle()
+            Player.instance.puzzle = this;
+            //start animation state for starting a puzzle
+            Player.instance.animstate.SetBool("IsInPuzzle", Player.instance.isInPuzzle);
 
-			//Place the difficulty values in a NextSceneManager var. So when the loaded puzzle scene
-			//is loaded in its Awake() function it will get the NextSceneManager var and set that difficulity 
-			NextSceneManager.instance.setPuzzledifficulty = difficultyNeed;
-			//pass this current placeholder instance for actions changes when the puzzle is complete
-			NextSceneManager.instance.placeholder = this;
-
-			//Load Puzzle Scene on top the current scene
-			NextSceneManager.instance.LoadPuzzleScene (puzzleGoTo);
-		}
+        }
 
 	}
+    //Load the puzzle scene
+    public void GoToPuzzle() {
+        //Place the difficulty values in a NextSceneManager var. So when the loaded puzzle scene
+        //is loaded in its Awake() function it will get the NextSceneManager var and set that difficulity 
+        NextSceneManager.instance.setPuzzledifficulty = difficultyNeed;
+        //pass this current placeholder instance for actions changes when the puzzle is complete
+        NextSceneManager.instance.placeholder = this;
+
+        //Load Puzzle Scene on top the current scene
+        NextSceneManager.instance.LoadPuzzleScene(puzzleGoTo);
+    }
+
+    //Unload the puzzle scene and do the appropriate calls and sets. When a puzzle has exited
+    public void PuzzleExit(bool isCompleted)
+    {
+        //set the bool that the player is not in a puzzle
+        Player.instance.isInPuzzle = false;
+        Player.instance.puzzle = null;
+        Player.instance.animstate.SetBool("IsInPuzzle", false);
+        
+        //unLocking the player's movement will be handled by the animator 
+
+        if (isCompleted)
+        {
+            //TODO: add the other updates it needs to do when the puzzle is completed
+
+               //Door unlock
+            if (door != null)
+            {
+                //set the bool to allow the door to open
+               door.DoorLockChange(true);
+            }
+        }
+        //Unload Puzzle scene
+        NextSceneManager.instance.UnloadPuzzleScene(puzzleGoTo);
+    }
 		
 }
