0e3d5f8b4ec1a46e4918e800d725fdab7d17d413
Leonel Jara <leoneljara3@gmail.com>
2018-03-18 14:10:21 -0400

    Scene transition - FadeIn / FadeOut

--- Assets/Scripts/ObjectScripts/Door.cs
+++ Assets/Scripts/ObjectScripts/Door.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 /*This Door class will only be used for door that switch to another level*/
 public class Door : MonoBehaviour {
 
 	public string levelName;
-	public bool isDoorlocked = false; 
+	public bool isDoorlocked = false;
 
+    public GameObject fadeObject; // ref of the fade UI object. This must be filled in the inspecter
 
-	//TODO: Complete door locking / unlocking
+    //Needed fade vars
+    private Animator fader;
+    private Image im;
 
-	void OnTriggerStay2D(Collider2D col)
+    //TODO: Complete door locking / unlocking
+
+    void Awake()
+    {
+        fader = fadeObject.GetComponent<Animator>();
+        im = fadeObject.GetComponent<Image>();
+    }
+
+    void OnTriggerStay2D(Collider2D col)
 	{
 		//if a player is on the trigger and is pressing the action buttion it will go to next scene
-		if(col.gameObject.name.Equals("Player(Clone)") && Player.instance.actionButtion) 
+		if(col.gameObject.CompareTag("Player") && Player.instance.actionButtion) 
 		{
-			//Load next scene
-			NextSceneManager.instance.LoadLevelScene (levelName);
+            if (!fader.GetBool("Fade")) { // make sure we don't keep calling the load level scene while the scene is still the fade transition
+                NextSceneManager.instance.LoadLevelScene(levelName, fader, im);
+            }
+            
 		}
-
 	}
-
-
-
-
 }
