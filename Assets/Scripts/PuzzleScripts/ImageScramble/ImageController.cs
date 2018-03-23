2c468ebf59d9a6bff4424360e4ef2bf5a87e2192
Jyrah <Jyrahscope@gmail.com>
2018-03-23 18:13:06 -0400

    Added ImageScramble

---
+++ Assets/Scripts/PuzzleScripts/ImageScramble/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageController : MonoBehaviour {
+
+    public GameObject target;
+    public bool startMove = false;
+
+    GameController gameMN;
+
+	// Use this for initialization
+	void Start () {
+        GameObject gamemanager = GameObject.Find ("GameController");
+        gameMN = gamemanager.GetComponent<GameController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startMove) {
+            startMove = false;
+            this.transform.position = target.transform.position; // move to new position
+            gameMN.checkComplete = true;
+        }
+	}
+}
