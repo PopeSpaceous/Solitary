1a552e617953afe8792dd0c360c8ba172cb5f458
anthonynguyen.15 <anthonynguyen435@gmail.com>
2018-03-24 12:36:42 -0400

    initial math game puzzle

---
+++ Assets/Scripts/PuzzleScripts/WordPasscode/Questions.cs
@@ -0,0 +1,17 @@
+﻿
+[System.Serializable]
+public class Questions {
+
+    //store question
+    public string question;
+    //store possibilties
+    public string a;
+    public string b;
+    public string c;
+    public string d;
+    //store answer
+    public string answer;
+
+    public int totalQuestionsAnswered = 0;
+    public int totalErrors = 0;
+}
