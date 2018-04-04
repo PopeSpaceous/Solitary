dfba26f10811b4154363d6c97b8b0ae08e0fc864
PopeSpaceous <nathan_misener@hotmail.com>
2018-04-03 22:18:55 -0400

    Changed Button name

--- Assets/Scripts/PuzzleScripts/Anagram/CheckSolution.cs
+++ Assets/Scripts/PuzzleScripts/Anagram/CheckSolution.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class CheckButton : MonoBehaviour {
+public class CheckSolution : MonoBehaviour {
 	public Anagram myAnagram;
 	public Button myButt;
 
