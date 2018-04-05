8abbb9bef240a5ddc3a0364c764db66b00966cf7
PopeSpaceous <nathan_misener@hotmail.com>
2018-04-05 00:15:16 -0400

    Added computer screen, computer terminal animation, text animation

---
+++ Assets/Scripts/ObjectScripts/ComputerTerminal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerTerminal : MonoBehaviour {
+	public Animator ani;
+	public ComputerScreen comp;
+	// Use this for initialization
+	void Start () {
+		ani.speed = 0.25f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.CompareTag ("Player") && Player.instance.actionButtion) {
+			//comp.toggleView();
+			comp.toggleView();
+		}
+	}
+}
