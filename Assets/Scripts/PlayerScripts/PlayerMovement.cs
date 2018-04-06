a3838fb637be04da3b4169f9df715862183c33d1
PopeSpaceous <nathan_misener@hotmail.com>
2018-04-06 00:30:01 -0400

    Added door sfx, lift sfx, player walk sfx, jump and jump land sfx

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-	
+
 	//For Walking
 	public float movementSpeed;
-
+	public List<AudioSource> sounds = new List<AudioSource> ();
+	bool playedOnce=false;
 	//For Jumping
 	public float jumpForce; 
 	public float jumpAmmount = 200f; // jumpAmmount is the max ammount of jump the character has.
@@ -16,7 +17,8 @@ public class PlayerMovement : MonoBehaviour {
     public float nextJumpDelay = 0.2f;
     private bool jumpStarted = false;
     private float currentJumpAmmount = 0;   // currentJumpAmmount is the current ammount left while the character is jumping. This can be use for jump height
-
+	bool jumpEnd = true;
+	bool inAir =true;
     public Transform groundCheck;
     public LayerMask whatsGround;
     public float hardLandingThreshold = -16f;
@@ -30,6 +32,7 @@ public class PlayerMovement : MonoBehaviour {
 		//Gets the references
 		rigBod = GetComponent<Rigidbody2D>();
         sR = GetComponent<SpriteRenderer>();
+		sounds = new List<AudioSource>( GetComponents<AudioSource> ());
     }
 
 	//FixedUpdate will handle the response from the user input for jumping and walking
@@ -44,7 +47,6 @@ public class PlayerMovement : MonoBehaviour {
         {
             currentJumpAmmount = jumpAmmount;
             StartCoroutine(JumpDelay());
-
         }
         else if (isJumping) // keep calling jump until the jump has ended
         {
@@ -56,13 +58,22 @@ public class PlayerMovement : MonoBehaviour {
         Player.instance.animstate.SetFloat("JPotential", currentJumpAmmount + rigBod.velocity.y);
         Player.instance.animstate.SetBool("JumpStarted", jumpStarted);
         Player.instance.animstate.SetBool("IsJumping", isJumping);
+
+		if (!sounds [2].isPlaying && !isJumping && jumpEnd && isGrounded && rigBod.velocity.y==0 && !isJumping ) {
+			sounds [2].Play();
+			jumpEnd = false;
+			inAir = false;
+		}
         //Hard Landing check
         if (rigBod.velocity.y <= hardLandingThreshold) {
             Player.instance.animstate.SetBool("LandHard", true);
+
+			playedOnce = false;
         }
         else if (Player.instance.animstate.GetCurrentAnimatorStateInfo(0).IsTag("Idle") || jumpStarted) { //will reset the HardLand bool if an Idle or new jump has been set
-
             Player.instance.animstate.SetBool("LandHard", false);
+			playedOnce = false;
+
         }
 
         //Response for horizontal movement
@@ -84,8 +95,14 @@ public class PlayerMovement : MonoBehaviour {
         {
             sR.flipX = false;
         }
-        Player.instance.animstate.SetFloat("WalkState", Mathf.Abs(Player.instance.horizontalInput));
 
+			Player.instance.animstate.SetFloat ("WalkState", Mathf.Abs (Player.instance.horizontalInput));
+		if (!sounds[0].isPlaying && Player.instance.horizontalInput!=0 ) {
+			sounds[0].Play ();
+		}
+		if (Player.instance.horizontalInput == 0||!isGrounded) {
+			sounds[0].Stop ();
+		}
         //Debuging Only
         //if (rigBod.velocity.y != 0) {
             //Debug.Log("Potential: " + currentJumpAmmount + rigBod.velocity.y + " Vel: " + rigBod.velocity.y);
@@ -98,18 +115,31 @@ public class PlayerMovement : MonoBehaviour {
         //Response for jumping. If the user is pressing the jump buttion and there is an ammount for a jump, the actual jump will trigger.
         if (Player.instance.jumpInput && currentJumpAmmount > 0)
         {
+			if (!sounds [1].isPlaying&&!playedOnce && !isGrounded) {
+				playedOnce = true;
+				sounds [1].Play();
+			}
             rigBod.velocity = new Vector2(rigBod.velocity.x, jumpForce); // Add a bit of y velocity 
             isJumping = true;
             currentJumpAmmount -= decreaseAmmountBy;
+			inAir = true;
         }
         else
         {
+			if (!sounds [1].isPlaying&&!playedOnce && !isGrounded) {
+				playedOnce = true;
+				sounds [1].Play();
+			}
             isJumping = false;
             currentJumpAmmount = 0;
         }
         //This if will cancel the jump if the character has hit his head on a object.
         if (isJumping && rigBod.velocity.y == 0)
         {
+			if (!sounds [1].isPlaying&&!playedOnce && !isGrounded) {
+				playedOnce = true;
+				sounds [1].Play();
+			}
             isJumping = false;
             currentJumpAmmount = 0;
         }
@@ -117,10 +147,13 @@ public class PlayerMovement : MonoBehaviour {
 
     IEnumerator JumpDelay() {
         jumpStarted = true;
+
         yield return new WaitForSeconds(nextJumpDelay);
         // start jump
         RunJump(); 
+		jumpEnd = true;
         jumpStarted = false;
+
     }
     void OnDrawGizmos()
     {
